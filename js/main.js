f8f53b0e719e91b8e50133ecf9a8b4cfe211261a
Stefan Rosu <rosus89@gmail.com>
2019-05-29 16:27:39 +0100

    testing template

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ const panels = document.querySelectorAll('.panel');
 const about = document.querySelector('.about');
 const work = document.querySelector('.work');
 const contact = document.querySelector('.contact');
+const aboutHTML = document.querySelector('#aboutHTML').import;
 
 function togglePanel() {
     panels.forEach(panel => panel.classList.remove('open'));
     panels.forEach(panel => panel.addEventListener('click', togglePanel));
     this.classList.toggle('open');
     this.removeEventListener('click', togglePanel);
+    if (this.classList.contains('about') === true){
+        this.innerHTML= aboutHTML;
+    }
 }
 
 panels.forEach(panel => panel.addEventListener('click', togglePanel));
