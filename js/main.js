a97e762933a5141126a56c082d4dc526698f9770
Stefan Rosu <rosus89@gmail.com>
2019-06-05 15:03:36 +0100

    Style: started adding transition for panel content

--- js/main.js
+++ js/main.js
@@ -5,9 +5,24 @@ const about = document.querySelector('.about');
 const work = document.querySelector('.work');
 const contact = document.querySelector('.contact');
 
+let state = {
+    about: 0,
+    work: 0,
+    contact: 0
+}
+
+let reset = {
+    stateReset: JSON.parse(JSON.stringify(state)),
+    init: function() {
+        state = this.stateReset;
+    }
+}
+
+
 const aboutPage = document.querySelector('#aboutPage');
 const workPage = document.querySelector('#workPage');
 const contactPage = document.querySelector('#contactPage');
+
 
 function togglePanel() {
     panels.forEach(panel => panel.classList.remove('open'));
@@ -22,6 +37,8 @@ function render(element) {
         work.innerHTML = "My Work";
         contact.innerHTML = "Contact";
         element.innerHTML = aboutPage.innerHTML;
+        console.log(element);
+        element.innerHTML.classList.add('active');
     } else if (element.classList.contains('work') === true) {
         contact.innerHTML = "Contact";
         about.innerHTML = "About"
